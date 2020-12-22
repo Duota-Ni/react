268b00ebd97e3233d3572377d6dee74ff956ad6c
黎宛婷 <liwanting@do1.com.cn>
2020-12-22 18:59:04 +0800

    vueBook

---
+++ vueBook/chapter02/objectChangeDetection.js
@@ -0,0 +1,16 @@
+// Object.defineProperty()封装
+function defineReactive (data, key, val) {
+  Object.defineProperty (data,key,{
+    enumerable:true,
+    configurable:true,
+    get () {
+      return val
+    },
+    set (newVal) {
+      if (val === newVal) {
+        return
+      }
+      val = newVal
+    }
+  })
+}
