d33151da34f4d2fd3900870807d7e387c1a3cfa5
Duota <duota001@163.com>
2021-01-22 20:26:19 +0800

    newlist

--- nodeDemo/新闻列表/index.js
+++ nodeDemo/新闻列表/index.js
@@ -8,9 +8,8 @@ const cheerio = require("cheerio");
 const server = http.createServer((req, res) => {
     res.setHeader("content-type", "text/html;charset=utf-8");
     //    res.writeHead(300,{"content-type":"text/html;charset=utf-8"})
-    console.log(req.url);
-    let urlObj = url.parse(req.url);
-    console.log(urlObj);
+    let urlObj = url.parse(req.url,true);
+    // console.log(urlObj);
     // 作业实现分页；每页 5条数据；（选做）;
     // 接收get参数：urlObj.query;
     // p = 1 ;perPage = 5; --->start: (p-1)*perPage perPage ; splice
@@ -23,14 +22,18 @@ const server = http.createServer((req, res) => {
         // 流方式；
         // 组装html；
         let str = "";
-        data.forEach(v => {
+        let p = parseInt(urlObj.query.p) || 1
+        let perPage = 5
+        let newData = JSON.parse(JSON.stringify(data)).splice((p-1)*perPage,perPage)
+        let pageCount = Math.ceil((JSON.parse(JSON.stringify(data)).length)/perPage)
+        newData.forEach(v => {
             str += `<li class="news">
             <a href="javascript:;">
                 <img src="${v.imgUrl}" alt="">
             </a>
             <div>
                 <h3>
-                    <a href="javascript:;">${v.title}</a>
+                    <a href="/detail?id=${v.id}">${v.title}</a>
                 </h3>
                 <div class="info">
                     <span class="tips"><span>${v.from}</span></span>
@@ -44,14 +47,32 @@ const server = http.createServer((req, res) => {
         let indexData = fs.readFileSync("./views/index.html");
         let $ = cheerio.load(indexData);
         $(".news-list").html(str);
+        // 组装分页html
+        let pageHtml = `<a href="/index?p=${p<=1?1:(p-1)}" class="prev">⌜</a>`
+        for(let i = 1;i<=pageCount;i++){
+          pageHtml += `<a href="/index?p=${i}">${i}</a>`
+        }
+        pageHtml += `<a href="/index?p=${p>=pageCount?pageCount:(p+1)}" class="next">⌝</a>`
+        $(".pagination").html(pageHtml)
         // let indexData = fs.createReadStream("./views/index.html");
         // indexData.pipe(res);
         res.end($.html());
     } else if (urlObj.pathname === "/detail") {
         // let indexData =  fs.readFileSync("./views/product.html")
-        // res.end(indexData);
-        let indexData = fs.createReadStream("./views/detail.html");
-        indexData.pipe(res);
+        // res.end(indexData);dv
+        let id = parseInt(urlObj.query.id) || 1
+        let detailData = JSON.parse(JSON.stringify(data)).filter(v=>v.id == id)[0]
+        let indexData = fs.readFileSync("./views/detail.html")
+        let $ = cheerio.load(indexData)
+        let str = `<h1 class="title">${detailData.title}</h1>
+        <div class="article-info"> ${detailData.from} 时间：${detailData.newTime}</div>
+        <p class="content">
+        ${detailData.title}
+        </p>`
+        $(".text").html(str)
+        res.end($.html())
+        // let indexData = fs.createReadStream("./views/detail.html")
+        // indexData.pipe(res);
     } else {
         if (urlObj.pathname !== "/favicon.ico") {
             // 获取扩展名；
